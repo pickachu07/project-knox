dd75c47f667648f695de12ad117f6f7bcf001298
pickachu07 <subhodip.kumar.7@gmail.com>
2020-07-14 21:20:54 +0530

    first commit

---
+++ src/test/java/com/srs/knox/KnoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srs.knox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
