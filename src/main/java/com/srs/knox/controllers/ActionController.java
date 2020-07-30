ae10c3b611b39dd4c9fd98b8535a58c5275e8943
Rohan Das <roh.das@outlook.com>
2020-07-30 11:37:04 +0530

    Adding cross origin annotation to resolve merge conflict.

--- src/main/java/com/srs/knox/controllers/ActionController.java
+++ src/main/java/com/srs/knox/controllers/ActionController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,6 +40,7 @@ public class ActionController {
 	ActionService actionService;
 	
 	@PostMapping(value="/create")
+	@CrossOrigin(origins = "http://localhost:3000")
 	@ResponseBody
 	public ResponseEntity<String> createAction(@RequestBody CreateActionRequest requestBody, @RequestHeader("vdpr_api_key") String key) {
 		ObjectMapper objectMapper = new ObjectMapper();
